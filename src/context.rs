3b5050cd0c0f178c7837c7828d3f5d379746fefa
Theo Garefalakis <tgaref@gmail.com>
2025-10-12 20:53:53 +0300

    Added Mine type to mine data and methods for building and executing

--- src/context.rs
+++ src/context.rs
@@ -1,4 +1,65 @@
+use anyhow::Result;
 use serde_yaml::Value;
 use std::collections::HashMap;
+use std::fs;
+use std::path::Path;
+
+use crate::rule::parse_front_matter;
+use crate::Document;
 
 pub type Variables = HashMap<String, Value>;
+pub type Miner = Box<dyn Fn(&Document) -> Variables>;
+
+pub type Data = HashMap<String, Variables>;
+
+pub struct Mine {
+    pub pattern: Vec<String>,
+    pub miners: Vec<Miner>,
+}
+
+impl Mine {
+    pub fn load(pattern: &[&str]) -> Self {
+        Mine {
+            pattern: pattern.iter().map(|pat| pat.to_string()).collect(),
+            miners: vec![],
+        }
+    }
+
+    pub fn miner(mut self, f: Miner) -> Self {
+        self.miners.push(f);
+        self
+    }
+
+    pub fn execute(&self, site_dir: &str) -> Result<Data> {
+        let walker = globwalk::GlobWalkerBuilder::from_patterns(site_dir, &self.pattern)
+            .follow_links(true)
+            .build()?
+            .filter_map(Result::ok);
+
+        let mut data = Data::new();
+        for entry in walker {
+            let path = entry.path();
+            if path.is_file() {
+                let path_str = path.to_str().unwrap().to_string();
+                // Load document
+                let doc = {
+                    let p = Path::new(&path_str).canonicalize().unwrap();
+                    let content = fs::read_to_string(p)?;
+                    let (metadata, body) = parse_front_matter(&content);
+                    Document {
+                        path: path_str.clone(),
+                        content: body,
+                        metadata,
+                    }
+                };
+
+                let mut ctx = Variables::new();
+                for f in &self.miners {
+                    ctx.extend(f(&doc));
+                }
+                data.insert(path_str, ctx);
+            }
+        }
+        Ok(data)
+    }
+}
